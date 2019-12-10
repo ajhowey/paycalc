654326f9ae2bd3dd3e627c057ff747453cdedc07
Andy Howey <ajhowey@gmail.com>
2019-12-10 14:09:30 -0800

    Adds "clear screen" function for better readability

--- netpay_calculator.py
+++ netpay_calculator.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+
+# import only system from os 
+from os import system, name 
+  
+# define our clear function 
+def clear(): 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+  
+# now call function we defined above 
+clear() 
+
 H1 = 0
 OT1 = 0
 print("\n\n")
